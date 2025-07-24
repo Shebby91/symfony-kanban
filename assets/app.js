2b6fab4200ef6194be74337e9c3cf8c44135b36f
Shebby91 <sgrauthoff@gmail.com>
2025-07-24 11:03:17 +0200

    self hosted symbols local bootstrap

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,9 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import './vendor/bootstrap/css/bootstrap.min.css';
+import './vendor/bootstrap/js/bootstrap.bundle.min.js';
 import './styles/app.css';
-
+import './js/color-mode.js';
+import './js/board.js';
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
